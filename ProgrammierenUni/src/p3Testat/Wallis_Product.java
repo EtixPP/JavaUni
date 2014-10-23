63a7804019a1690e5c9a40427cb6ab9914ca2517
Etixpp <etix@foxfiredev.net>
2014-10-23 18:39:01 +0200

    Testat lösung

--- ProgrammierenUni/src/p3Testat/Wallis_Product.java
+++ ProgrammierenUni/src/p3Testat/Wallis_Product.java
@@ -3,7 +3,8 @@ import p3Testat.EAM;
 public class Wallis_Product {
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Geben sie ein n ein:");
 		long l_n = EAM.rLong();
 		double l_result = 2;
@@ -15,10 +16,43 @@ public class Wallis_Product {
 			if(l_bottom > l_top) l_top+=2;
 			else if(l_bottom < l_top) l_bottom+=2;
 		}
-		System.out.println("Näherungswert für Pi ist: "+ 2* l_result);
+		System.out.println("Näherungswert für Pi (iterativ berechnet) ist: "+ 2* l_result);
+		try
+		{
+			System.out.println("Näherungswert für Pi (rekursiv berechnet) ist: "+ 2* ValRec(l_n,false,2.0,1.0));
+		}
+		catch(StackOverflowError e)
+		{
+			System.out.println("Zu auslastend für die rekursive Variante :( (so bis 3000 +- 1000 gehts, zmd. auf meinem Rechner)");
+		}
 
 		
 
 	}
-
+	public static Double ValRec(double p_Num, boolean p_switch, double p_top, double p_bot) // Nur ein bisschen rumgespielt, höchstwahrscheinlich eher mäßig effektiv implementiert.
+	{
+		if(p_Num > 0)
+		{
+			boolean l_switch = p_switch;
+			double l_result = p_top/p_bot;
+			if(l_switch) p_top += 2; 
+			else p_bot +=2;
+			return l_result * ValRec (p_Num-1,!l_switch,p_top,p_bot);	
+			/*
+			 * 
+			 * 	Alte Variante, oben so weit optimiert, dass er deutlich höhere Zahlen handlen kann.	(bis zu 6000 anstatt 3000!!! Hab noch überlegt mit float anstatt double
+			 * zu arbeiten da es nur 4 bytes zieht, aber das würde dann zu sehr auf die Genauigkeit gehen.
+			boolean l_switch = p_switch;
+			double l_top = p_top;
+			double l_bot = p_bot;
+			double l_result = l_top/l_bot;
+			if(l_switch) l_top += 2; 
+			else l_bot +=2;
+			return l_result * ValRec (p_Num-1,!l_switch,l_top,l_bot);	
+			 * 
+			 */
+		}
+		else
+			return 1.0;
+	}
 }
